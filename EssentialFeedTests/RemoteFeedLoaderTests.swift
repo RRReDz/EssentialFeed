b1ae91b454000c1a659c10c82741b5ca703f1f4f
Riccardo Rossi <r.rossi@reply.it>
2021-02-12 19:50:11 +0100

    Refactored a test to capture more that one error

--- EssentialFeedTests/RemoteFeedLoaderTests.swift
+++ EssentialFeedTests/RemoteFeedLoaderTests.swift
@@ -48,16 +48,14 @@ class RemoteFeedLoaderTests: XCTestCase {
     func test_load_deliversErrorOnClientError() {
         // Given
         let (sut, client) = makeSUT()
-        var capturedError: RemoteFeedLoader.Error?
+        var capturedErrors: [RemoteFeedLoader.Error] = []
         
         // When
-        sut.load { error in
-            capturedError = error
-        }
+        sut.load { capturedErrors.append($0) }
         client.completions[0](NSError(domain: "MyError", code: 1, userInfo: nil))
         
         // Then
-        XCTAssertEqual(capturedError, .connectivity)
+        XCTAssertEqual(capturedErrors, [.connectivity])
     }
     
     private func makeSUT(url: URL = URL(string: "http://another-fantastic-awesome-url.com")!) -> (sut: RemoteFeedLoader, client: HTTPClientSpy) {
