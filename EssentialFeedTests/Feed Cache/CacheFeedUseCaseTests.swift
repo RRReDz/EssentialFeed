1eb1d7b81e8e82957012a691383401418b315500
Riccardo Rossi <riki.redz@gmail.com>
2021-03-07 13:14:56 +0100

    Extract `FeedCacheSpy` into its own file.

--- EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
+++ EssentialFeedTests/Feed Cache/CacheFeedUseCaseTests.swift
@@ -101,48 +101,6 @@ class CacheFeedUseCaseTests: XCTestCase {
     }
     
     // MARK: - Helpers
-    class FeedStoreSpy: FeedStore {
-        enum ReceivedMessage: Equatable {
-            case insert([LocalFeedImage], Date)
-            case deleteCacheFeed
-        }
-        
-        private(set) var receivedMessages: [ReceivedMessage] = []
-        
-        var deletionCompletions: [DeletionCompletion] = []
-        var insertionCompletions: [InsertionCompletion] = []
-        
-        // Called by LocalFeedLoader
-        
-        func deleteCachedFeed(completion: @escaping DeletionCompletion) {
-            deletionCompletions.append(completion)
-            receivedMessages.append(.deleteCacheFeed)
-        }
-        
-        func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
-            insertionCompletions.append(completion)
-            receivedMessages.append(.insert(feed, timestamp))
-        }
-        
-        // Called by Tests
-        
-        func completeDeletion(with error: Error, at index: Int = 0) {
-            deletionCompletions[index](error)
-        }
-        
-        func completeInsertion(with error: Error, at index: Int = 0) {
-            insertionCompletions[index](error)
-        }
-        
-        func completeDeletionWithSuccess(at index: Int = 0) {
-            deletionCompletions[index](nil)
-        }
-        
-        func completeInsertionWithSuccess(at index: Int = 0) {
-            insertionCompletions[index](nil)
-        }
-    }
-    
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalFeedLoader, store: FeedStoreSpy) {
         let store = FeedStoreSpy()
         let sut = LocalFeedLoader(store: store, currentDate: currentDate)
---
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -0,0 +1,51 @@
+//
+//  FeedStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Riccardo Rossi - Home on 07/03/21.
+//
+
+import Foundation
+import EssentialFeed
+
+internal class FeedStoreSpy: FeedStore {
+    internal enum ReceivedMessage: Equatable {
+        case insert([LocalFeedImage], Date)
+        case deleteCacheFeed
+    }
+    
+    private(set) var receivedMessages: [ReceivedMessage] = []
+    
+    internal var deletionCompletions: [DeletionCompletion] = []
+    internal var insertionCompletions: [InsertionCompletion] = []
+    
+    // Called by LocalFeedLoader
+    
+    internal func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+        deletionCompletions.append(completion)
+        receivedMessages.append(.deleteCacheFeed)
+    }
+    
+    internal func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+        insertionCompletions.append(completion)
+        receivedMessages.append(.insert(feed, timestamp))
+    }
+    
+    // Called by Tests
+    
+    internal func completeDeletion(with error: Error, at index: Int = 0) {
+        deletionCompletions[index](error)
+    }
+    
+    internal func completeInsertion(with error: Error, at index: Int = 0) {
+        insertionCompletions[index](error)
+    }
+    
+    internal func completeDeletionWithSuccess(at index: Int = 0) {
+        deletionCompletions[index](nil)
+    }
+    
+    internal func completeInsertionWithSuccess(at index: Int = 0) {
+        insertionCompletions[index](nil)
+    }
+}
