e7d9d84e09e9fa61dd29a1395374d37329d9bf95
Riccardo Rossi <riki.redz@gmail.com>
2021-04-03 18:16:47 +0200

    Remove internal access control declarations.

--- EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
+++ EssentialFeedTests/Feed Cache/Helpers/FeedStoreSpy.swift
@@ -8,8 +8,8 @@
 import Foundation
 import EssentialFeed
 
-internal class FeedStoreSpy: FeedStore {
-    internal enum ReceivedMessage: Equatable {
+class FeedStoreSpy: FeedStore {
+    enum ReceivedMessage: Equatable {
         case insert([LocalFeedImage], Date)
         case deleteCacheFeed
         case retrieve
@@ -17,54 +17,54 @@ internal class FeedStoreSpy: FeedStore {
     
     private(set) var receivedMessages: [ReceivedMessage] = []
     
-    internal var deletionCompletions: [DeletionCompletion] = []
-    internal var insertionCompletions: [InsertionCompletion] = []
-    internal var retrievalCompletions: [RetrievalCompletion] = []
+    var deletionCompletions: [DeletionCompletion] = []
+    var insertionCompletions: [InsertionCompletion] = []
+    var retrievalCompletions: [RetrievalCompletion] = []
     
     // Called by LocalFeedLoader
     
-    internal func deleteCachedFeed(completion: @escaping DeletionCompletion) {
+    func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         deletionCompletions.append(completion)
         receivedMessages.append(.deleteCacheFeed)
     }
     
-    internal func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
+    func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         insertionCompletions.append(completion)
         receivedMessages.append(.insert(feed, timestamp))
     }
     
-    internal func retrieve(completion: @escaping RetrievalCompletion) {
+    func retrieve(completion: @escaping RetrievalCompletion) {
         retrievalCompletions.append(completion)
         receivedMessages.append(.retrieve)
     }
     
     // Called by Tests
     
-    internal func completeDeletion(with error: Error, at index: Int = 0) {
+    func completeDeletion(with error: Error, at index: Int = 0) {
         deletionCompletions[index](error)
     }
     
-    internal func completeInsertion(with error: Error, at index: Int = 0) {
+    func completeInsertion(with error: Error, at index: Int = 0) {
         insertionCompletions[index](error)
     }
     
-    internal func completeDeletionWithSuccess(at index: Int = 0) {
+    func completeDeletionWithSuccess(at index: Int = 0) {
         deletionCompletions[index](nil)
     }
     
-    internal func completeInsertionWithSuccess(at index: Int = 0) {
+    func completeInsertionWithSuccess(at index: Int = 0) {
         insertionCompletions[index](nil)
     }
     
-    internal func completeRetrieval(with error: Error, at index: Int = 0) {
+    func completeRetrieval(with error: Error, at index: Int = 0) {
         retrievalCompletions[index](.failure(error))
     }
     
-    internal func completeRetrievalSuccessfullyWithEmptyCache(at index: Int = 0) {
+    func completeRetrievalSuccessfullyWithEmptyCache(at index: Int = 0) {
         retrievalCompletions[index](.empty)
     }
     
-    internal func completeRetrieval(with localFeed: [LocalFeedImage], timestamp: Date, at index: Int = 0) {
+    func completeRetrieval(with localFeed: [LocalFeedImage], timestamp: Date, at index: Int = 0) {
         retrievalCompletions[index](.found(feed: localFeed, timestamp: timestamp))
     }
 }
