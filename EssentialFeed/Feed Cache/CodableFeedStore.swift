e93f1ad707a36184acf4bbf19c2f6dcd333fdabd
Riccardo Rossi <riki.redz@gmail.com>
2021-04-04 12:06:54 +0200

    Simplify `CodableFeedStore` completion code with the new `Result` APIs

--- EssentialFeed/Feed Cache/CodableFeedStore.swift
+++ EssentialFeed/Feed Cache/CodableFeedStore.swift
@@ -49,45 +49,36 @@ public class CodableFeedStore: FeedStore {
     
     public func retrieve(completion: @escaping RetrievalCompletion) {
         queue.async { [storeURL] in
-            guard let data = try? Data(contentsOf: storeURL) else {
-                completion(.success(.none))
-                return
-            }
-            do {
+            completion(Result {
+                guard let data = try? Data(contentsOf: storeURL) else {
+                    return .none
+                }
                 let decoder = JSONDecoder()
                 let cache = try decoder.decode(Cache.self, from: data)
-                completion(.success(.some((feed: cache.localFeed, timestamp: cache.timestamp))))
-            } catch {
-                completion(.failure(error))
-            }
+                return (feed: cache.localFeed, timestamp: cache.timestamp)
+            })
         }
     }
     
     public func insert(_ feed: [LocalFeedImage], timestamp: Date, completion: @escaping InsertionCompletion) {
         queue.async(flags: .barrier) { [storeURL] in
-            do {
+            completion(Result {
                 let encoder = JSONEncoder()
                 let cache = Cache(feed: feed.map(CodableFeedImage.init), timestamp: timestamp)
                 let encoded = try encoder.encode(cache)
                 try encoded.write(to: storeURL)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
     
     public func deleteCachedFeed(completion: @escaping DeletionCompletion) {
         queue.async(flags: .barrier) { [storeURL] in
-            guard FileManager.default.fileExists(atPath: storeURL.path) else {
-                return completion(.success(()))
-            }
-            do {
+            completion(Result {
+                guard FileManager.default.fileExists(atPath: storeURL.path) else {
+                    return
+                }
                 try FileManager.default.removeItem(at: storeURL)
-                completion(.success(()))
-            } catch {
-                completion(.failure(error))
-            }
+            })
         }
     }
 }
