eb5404add4ef48280b628ba9d07722158259b254
Riccardo Rossi <riki.redz@gmail.com>
2021-04-12 23:14:06 +0200

    Extract FeedViewController test helpers to a separate file

--- EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
+++ EssentialFeediOSTests/Controllers/FeedViewControllerTests.swift
@@ -334,55 +334,6 @@ class FeedViewControllerTests: XCTestCase {
     }
 }
 
-private extension FeedViewController {
-    func simulateUserInitiatedFeedReload() {
-        refreshControl?.simulatePullToRefresh()
-    }
-    
-    @discardableResult
-    func simulateFeedImageViewVisible(at index: Int) -> FeedImageCell? {
-        return feedImageView(at: index) as? FeedImageCell
-    }
-    
-    func simulateFeedImageViewNotVisible(at index: Int) {
-        let cell = simulateFeedImageViewVisible(at: index)!
-        
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        let delegate = tableView.delegate
-        delegate?.tableView?(tableView, didEndDisplaying: cell, forRowAt: indexPath)
-    }
-    
-    func simulateFeedImageViewNearVisible(at index: Int = 0) {
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        let ds = tableView.prefetchDataSource
-        ds?.tableView(tableView, prefetchRowsAt: [indexPath])
-    }
-    
-    func simulateFeedImageViewNotNearVisible(at index: Int = 0) {
-        simulateFeedImageViewNearVisible(at: index)
-        
-        let indexPath = IndexPath(row: index, section: feedImagesSection)
-        let ds = tableView.prefetchDataSource
-        ds?.tableView?(tableView, cancelPrefetchingForRowsAt: [indexPath])
-    }
-    
-    func isShowingLoadingIndicator() -> Bool {
-        return refreshControl!.isRefreshing
-    }
-    
-    private var feedImagesSection: Int { return 0 }
-    
-    func numberOfRenderedFeedImageViews() -> Int {
-        return tableView.numberOfRows(inSection: feedImagesSection)
-    }
-    
-    func feedImageView(at row: Int) -> UITableViewCell? {
-        let indexPath = IndexPath(row: row, section: feedImagesSection)
-        let ds = tableView.dataSource
-        return ds?.tableView(tableView, cellForRowAt: indexPath)
-    }
-}
-
 private extension FeedImageCell {
     func simulateRetryAction() {
         feedImageRetryButton.simulateTap()
