a11bf2688359f7b32ee171211ed4c9937d569898
Riccardo Rossi <riki.redz@gmail.com>
2021-04-19 22:50:17 +0200

    Move memory management to Composer layer. The `FeedPresenter` shouldn't have to know or handle their dependencies lifetime.

--- EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
+++ EssentialFeediOS/FeedUI/Composers/FeedUIComposer.swift
@@ -17,10 +17,24 @@ public final class FeedUIComposer {
         let refreshController = FeedRefreshController(presenter: presenter)
         
         let feedController = FeedViewController(refreshController: refreshController)
-        presenter.loadingView = refreshController
+        presenter.loadingView = WeakRefVirtualProxy(refreshController)
         presenter.feedView = FeedViewAdapter(feedController: feedController, loader: imageLoader)
         
         return feedController
+    }
+}
+
+private final class WeakRefVirtualProxy<T: AnyObject> {
+    private weak var object: T?
+    
+    init(_ object: T) {
+        self.object = object
+    }
+}
+
+extension WeakRefVirtualProxy: FeedLoadingView where T: FeedLoadingView {
+    func display(isLoading: Bool) {
+        object?.display(isLoading: isLoading)
     }
 }
 
