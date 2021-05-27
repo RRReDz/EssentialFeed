a95ce0fb2b50d29ceb865f4bb37016968c543d5a
Riccardo Rossi <riki.redz@gmail.com>
2021-05-27 23:37:53 +0200

    `FeedPresenter` sends loading message to the view when `didStartLoadingFeed` is called.

--- EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedPresenterTests.swift
@@ -7,20 +7,53 @@
 
 import XCTest
 
+protocol FeedLoadingView {
+    func display(_ viewModel: FeedLoadingViewModel)
+}
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
+
 final class FeedPresenter {
-    init(view: Any) {}
+    private let loadingView: FeedLoadingView
+    
+    init(loadingView: FeedLoadingView) {
+        self.loadingView = loadingView
+    }
+    
+    func didStartLoadingFeed() {
+        loadingView.display(FeedLoadingViewModel(isLoading: true))
+    }
 }
 
 class FeedPresenterTests: XCTestCase {
 
     func test_init_doesNotSendMessagesToView() {
         let view = ViewSpy()
-        _ = FeedPresenter(view: view)
+        _ = FeedPresenter(loadingView: view)
         
-        XCTAssertTrue(view.messages.isEmpty)
+        XCTAssertEqual(view.messages, [])
+    }
+    
+    func test_didStartLoadingFeed_sendLoadingMessageToLoadingView() {
+        let view = ViewSpy()
+        let sut = FeedPresenter(loadingView: view)
+        
+        sut.didStartLoadingFeed()
+        
+        XCTAssertEqual(view.messages, [.isLoading(true)])
     }
 
-    private class ViewSpy {
-        var messages = [Any]()
+    private class ViewSpy: FeedLoadingView {
+        enum Message: Equatable {
+            case isLoading(Bool)
+        }
+        
+        var messages = [Message]()
+        
+        func display(_ viewModel: FeedLoadingViewModel) {
+            messages.append(.isLoading(viewModel.isLoading))
+        }
     }
 }
