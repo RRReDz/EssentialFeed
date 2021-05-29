97901bb8c95de6ac4807b5d15cd1c73d9355735d
Riccardo Rossi <riki.redz@gmail.com>
2021-05-30 00:12:09 +0200

    Creates new `FeedImagePresenterTests`

---
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -0,0 +1,12 @@
+//
+//  FeedImagePresenterTests.swift
+//  EssentialFeedTests
+//
+//  Created by Riccardo Rossi - Home on 30/05/21.
+//
+
+import XCTest
+
+class FeedImagePresenterTests: XCTestCase {
+
+}
