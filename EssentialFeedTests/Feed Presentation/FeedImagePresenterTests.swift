1972c68f874a93d193cffa82ca97562445b7c8a9
Riccardo Rossi <riki.redz@gmail.com>
2021-05-30 01:07:24 +0200

    `FeedImagePresenter` asks view to display certain feed image representation when called `startLoadingImageData`

--- EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
+++ EssentialFeedTests/Feed Presentation/FeedImagePresenterTests.swift
@@ -6,27 +6,93 @@
 //
 
 import XCTest
+import EssentialFeed
 
-final class FeedImagePresenter {
-    init(view: Any) {}
+struct FeedImageViewModel<Image> {
+    let location: String?
+    let description: String?
+    let image: Image?
+    let isLoading: Bool
+    let retryLoading: Bool
+}
+
+protocol FeedImageView {
+    associatedtype Image
+    func display(_ viewModel: FeedImageViewModel<Image>)
+}
+
+final class FeedImagePresenter<ImageView: FeedImageView> {
+    private let imageView: ImageView
+    
+    init(imageView: ImageView) {
+        self.imageView = imageView
+    }
+    
+    func startLoadingImageData(for model: FeedImage) {
+        imageView.display(
+            FeedImageViewModel(
+                location: model.location,
+                description: model.description,
+                image: nil,
+                isLoading: true,
+                retryLoading: false))
+    }
 }
 
 class FeedImagePresenterTests: XCTestCase {
+    
     func test_init_doesNotSendMessageToView() {
         let (_, view) = makeSUT()
         
-        XCTAssert(view.messages.isEmpty)
+        XCTAssert(view.displayRepresentations.isEmpty)
     }
     
-    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedImagePresenter, ViewSpy) {
+    func test_startLoadingImageData_askViewToDisplayFeedImageRepresentation() {
+        let (sut, view) = makeSUT()
+        let image = uniqueImage()
+        
+        sut.startLoadingImageData(for: image)
+        
+        let feedImage = FeedImageRepresentation(
+            location: image.location,
+            description: image.description,
+            image: nil,
+            isLoading: true,
+            retryLoading: false)
+        XCTAssertEqual(view.displayRepresentations, [feedImage])
+    }
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> (FeedImagePresenter<ViewSpy>, ViewSpy) {
         let view = ViewSpy()
-        let sut = FeedImagePresenter(view: view)
+        let sut = FeedImagePresenter(imageView: view)
         trackForMemoryLeaks(sut, file: file, line: line)
         trackForMemoryLeaks(view, file: file, line: line)
         return (sut, view)
     }
     
-    private final class ViewSpy {
-        var messages = [Any]()
+    private final class ViewSpy: FeedImageView {
+        var displayRepresentations = [FeedImageRepresentation]()
+        
+        func display(_ viewModel: FeedImageViewModel<String>) {
+            displayRepresentations.append(FeedImageRepresentation(from: viewModel))
+        }
+    }
+}
+
+private struct FeedImageRepresentation: Equatable {
+    let location: String?
+    let description: String?
+    let image: String?
+    let isLoading: Bool
+    let retryLoading: Bool
+}
+
+extension FeedImageRepresentation {
+    init(from viewModel: FeedImageViewModel<String>) {
+        location = viewModel.location
+        description = viewModel.description
+        image = viewModel.image
+        isLoading = viewModel.isLoading
+        retryLoading = viewModel.retryLoading
     }
 }
