bb5bf62385ea20a89816915e2e810bb31b639414
Riccardo Rossi <riki.redz@gmail.com>
2021-06-06 21:14:01 +0200

    Removes `@discardableResult` and implementation related to "Liskov substitution principle" since unused.

--- EssentialFeed/Feed Feature/FeedImageDataLoader.swift
+++ EssentialFeed/Feed Feature/FeedImageDataLoader.swift
@@ -15,6 +15,5 @@ public protocol FeedImageDataLoader {
     typealias Result = Swift.Result<Data, Error>
     typealias Completion = (Result) -> Void
     
-    @discardableResult
     func loadImageData(from url: URL, completion: @escaping (Completion)) -> FeedImageDataLoaderTask
 }
