4a8bbbb13b39dff2856bd170e8935f8b114f046f
Riccardo Rossi <riki.redz@gmail.com>
2021-06-10 22:21:18 +0200

    Extract `FeedImageDataStoreSpy` into a shared scope

---
+++ EssentialFeedTests/Feed Cache/Helpers/FeedImageDataStoreSpy.swift
@@ -0,0 +1,36 @@
+//
+//  FeedImageDataStoreSpy.swift
+//  EssentialFeedTests
+//
+//  Created by Riccardo Rossi - Home on 10/06/21.
+//
+
+import Foundation
+import EssentialFeed
+
+final class FeedImageDataStoreSpy: FeedImageDataStore {
+    enum Message: Equatable {
+        case retrieve(dataFor: URL)
+        case insert(Data, for: URL)
+    }
+    
+    private(set) var messages = [Message]()
+    private var retrievalCompletions = [(FeedImageDataStore.RetrievalResult) -> Void]()
+
+    func retrieve(dataFor url: URL, completion: @escaping (FeedImageDataStore.RetrievalResult) -> Void) {
+        messages.append(.retrieve(dataFor: url))
+        retrievalCompletions.append(completion)
+    }
+    
+    func insert(_ data: Data, for url: URL) {
+        messages.append(.insert(data, for: url))
+    }
+    
+    func completeRetrieval(with error: Error, index: Int = 0) {
+        retrievalCompletions[index](.failure(error))
+    }
+    
+    func completeRetrieval(with data: Data?, index: Int = 0) {
+        retrievalCompletions[index](.success(data))
+    }
+}
