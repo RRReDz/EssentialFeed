1f0614942feae25199846a0b2c7e29c4bdd32d1a
Riccardo Rossi <riki.redz@gmail.com>
2021-06-27 17:14:06 +0200

    Record feed with content snapshot

--- EssentialFeediOSTests/FeedSnapshotTests.swift
+++ EssentialFeediOSTests/FeedSnapshotTests.swift
@@ -7,6 +7,7 @@
 
 import XCTest
 import EssentialFeediOS
+@testable import EssentialFeed
 
 class FeedSnapshotTests: XCTestCase {
     
@@ -16,6 +17,14 @@ class FeedSnapshotTests: XCTestCase {
         sut.display(emptyFeed())
         
         record(snapshot: sut.snapshot(), named: "EMPTY_FEED")
+    }
+    
+    func test_feedWithContent() {
+        let sut = makeSUT()
+        
+        sut.display(feedWithContent())
+        
+        record(snapshot: sut.snapshot(), named: "FEED_WITH_CONTENT")
     }
     
     private func makeSUT() -> FeedViewController {
@@ -52,6 +61,41 @@ class FeedSnapshotTests: XCTestCase {
         return []
     }
     
+    private func feedWithContent() -> [ImageStub] {
+        return [
+            ImageStub(
+                description: "The East Side Gallery is an open-air gallery in Berlin. It consists of a series of murals painted directly on a 1,316 m long remnant of the Berlin Wall, located near the centre of Berlin, on Mühlenstraße in Friedrichshain-Kreuzberg. The gallery has official status as a Denkmal, or heritage-protected landmark.",
+                location: "East Side Gallery\nMemorial in Berlin, Germany",
+                image: UIImage.make(withColor: .red)
+            ),
+            ImageStub(
+                description: "Garth Pier is a Grade II listed structure in Bangor, Gwynedd, North Wales.",
+                location: "Garth Pier",
+                image: UIImage.make(withColor: .green)
+            )
+        ]
+    }
+    
+}
+
+private class ImageStub: FeedImageCellControllerDelegate {
+    private let viewModel: FeedImageViewModel<UIImage>
+    weak var controller: FeedImageCellController?
+    
+    init(description: String, location: String, image: UIImage?) {
+        viewModel = FeedImageViewModel(
+            location: location,
+            description: description,
+            image: image,
+            isLoading: false,
+            retryLoading: image == nil)
+    }
+    
+    func didRequestImage() {
+        controller?.display(viewModel)
+    }
+    
+    func didCancelImageRequest() {}
 }
 
 private extension UIViewController {
@@ -60,5 +104,17 @@ private extension UIViewController {
         return renderer.image { action in
             view.layer.render(in: action.cgContext)
         }
+    }
+}
+
+private extension FeedViewController {
+    func display(_ imageStubs: [ImageStub]) {
+        let cellControllers: [FeedImageCellController] = imageStubs.map { imageStub in
+            let cellController = FeedImageCellController(delegate: imageStub)
+            imageStub.controller = cellController
+            return cellController
+        }
+        
+        display(cellControllers)
     }
 }
